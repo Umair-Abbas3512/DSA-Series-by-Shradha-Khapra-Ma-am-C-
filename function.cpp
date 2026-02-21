6612b4218a240f33851cd0f74f478992621a17ae
Umair Abbas Alhawachi <175593703+Umair-Abbas3512@users.noreply.github.com>
2026-02-21 20:26:04 +0500

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+// function
+int printHello(){
+    cout << "Hello Umari";
+    return 3;
+}
+int main(){
+
+// call / invoke
+count << printHello() << endl;
+return 0;
+}
---
+++ stack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int fun() {
+    int x = 25;
+    cout << "x" << x << endl;
+    return x;
+}
+
+int main() {
+    fun();
+    cout << x << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+// Stack:
+// Stack is a memory area where functions and local variables
+// are stored temporarily during program execution.
+
+// It follows LIFO (Last In, First Out) principle.
+// The last item added to the stack is the first one removed.
+
+// When a function finishes execution,
+// its local variables are automatically removed from the stack.
+
+// Short Definition:
+// Stack is temporary memory used during function execution
+// that follows the LIFO order.
+
+
+
+
+
+
